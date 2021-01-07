e8c5f6684090d50bbbdcc3f3a1139613eac6f5ed
Thomas <tommyprezioso@gmail.com>
2021-01-06 20:59:29 -0500

    Initial Commit

---
+++ SAAppetizers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAAppetizers
+//
+//  Created by Thomas Prezioso Jr on 1/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAAppetizers/SAAppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAAppetizersApp.swift
+//  SAAppetizers
+//
+//  Created by Thomas Prezioso Jr on 1/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SAAppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
